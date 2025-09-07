ec5b5d2d37df6bbfee42490cc779b3c80410dae5
Low-P1ng <namdevsurya14@gmail.com>
2025-09-07 17:30:09 +0530

    learning about models and DB

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 def items(request):
     return HttpResponse("This is items page you're seeing")
