2244446d213f3d819feb240f345df1465a136fc6
Low-P1ng <namdevsurya14@gmail.com>
2026-02-20 02:00:32 +0530

    experimented with logging/loggers

--- myapp/views.py
+++ myapp/views.py
@@ -9,13 +9,17 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator
 from django.views.decorators.cache import cache_page
+from django.shortcuts import get_object_or_404
+import logging
 
-
+logger = logging.getLogger(__name__)
 #@login_required
 # @cache_page(60 * 10)
 def index(request):
    #getting items from database
+   logger.info("Fethcing all items from the database.")
    item_list = Item.objects.all()
+   logger.debug(f"There are {item_list.count()} items.")
    paginator = Paginator(item_list, 5)
    page_number = request.GET.get("page")
    page_obj = paginator.get_page(page_number)
@@ -26,13 +30,22 @@ def index(request):
   # passing the context object to the render method along the template
    return render(request,"myapp/index.html",context)
 
-#def detail(request,id):
-#    #created a detailed veiw for each item
-#    item=Item.objects.get(id=id)
-#    context={
-#        'item':item
-#    }
-#    return render(request,"myapp/detail.html",context)
+def detail(request,id):
+    
+   #created a detailed veiw for each item
+    logger.info(f"Fetching an item with id:{id}")
+    logger.info(f"User {request.user} requested item from {request.META.get('REMOTE_ADDR')}")
+    try:
+        item = get_object_or_404(Item, pk=id)
+        logger.debug(f"Item found {item.item_name}")
+    except Exception as e:
+        logger.error("Error whiule fetching item with %s: %s",id,e)
+        raise
+
+    context={
+       'item':item
+    }
+    return render(request,"myapp/detail.html",context)
 
 # def create_item(request):
 #     form= Itemform(request.POST or None)
@@ -73,10 +86,10 @@ def index(request):
 #     context_object_name="item_list"
     
 
-class FoodDetail(DetailView):
-    model=Item
-    template_name="myapp/detail.html"
-    context_object_name="item"
+# class FoodDetail(DetailView):
+#     model=Item
+#     template_name="myapp/detail.html"
+#     context_object_name="item"
 
 class ItemCreateView(CreateView):
     #item_form.html
